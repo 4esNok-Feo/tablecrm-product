398bca5c7ede08ce5c37e57c574bd3a1cbc5acd7
4esNok-Feo <harrisonserg@gmail.com>
2026-03-10 20:33:52 +0300

    Update location-picker.tsx

--- src/components/product/location-picker.tsx
+++ src/components/product/location-picker.tsx
@@ -1,46 +1,20 @@
 "use client"
 
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-
-function LocationMarker({ setValue }: any) {
-
-  useMapEvents({
-    click(e) {
-      setValue("latitude", e.latlng.lat)
-      setValue("longitude", e.latlng.lng)
-    }
-  })
-
-  return null
-}
+import { useMapEvents } from "react-leaflet"
+import type { LeafletMouseEvent } from "leaflet"
 
 export default function LocationPicker({ form }: any) {
 
   const { setValue } = form
 
-  return (
+  useMapEvents({
+    click(e: LeafletMouseEvent) {
 
-    <div className="space-y-4">
+      setValue("latitude", e.latlng.lat)
+      setValue("longitude", e.latlng.lng)
 
-      <h2 className="text-lg font-semibold">
-        Локация
-      </h2>
+    }
+  })
 
-      <MapContainer
-        center={[55.75, 37.61]}
-        zoom={5}
-        style={{ height: 300 }}
-      >
-
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        <LocationMarker setValue={setValue} />
-
-      </MapContainer>
-
-    </div>
-  )
+  return null
 }
